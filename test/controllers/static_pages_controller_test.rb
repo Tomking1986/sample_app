174301d8aec2ce06275805f0555411e741ccbba1
tom <tomking1986@yahoo.co.uk>
2017-06-08 13:04:07 +0000

    finished static changes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,5 +23,16 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+  
+  # test "should get root" do
+  #   get :home
+  #   assert_response :success
+  # end
 
 end
