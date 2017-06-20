d5b0ba25ef4deb63ce37e6e8c7190e7f0459fbec
tom <tomking1986@yahoo.co.uk>
2017-06-20 14:45:03 +0000

    basic log-in finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,10 @@ class UsersController < ApplicationController
     puts params
     @user = User.new(user_params)
      if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
+      
     else
       render 'new'
     end
