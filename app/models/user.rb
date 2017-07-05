d5076dec8273d5f41f1548afdd10e091b119a03a
Tom King <tomking1986@yahoo.co.uk>
2017-07-05 12:09:41 +0200

    completed advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,16 @@ class User < ActiveRecord::Base
                     validates :password, presence: true, length: { minimum: 6 }
                     
                      # Returns a random token.
+    def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   def User.new_token
     SecureRandom.urlsafe_base64
   end
-   def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
+   
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
@@ -23,6 +27,11 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
   
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
    def forget
     update_attribute(:remember_digest, nil)
   end
