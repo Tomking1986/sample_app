fc2eece0784b1174c42c5f7f508f19574543e9fc
Tom King <tomking1986@yahoo.co.uk>
2017-07-05 16:53:42 +0200

    update complete

--- Gemfile
+++ Gemfile
@@ -46,6 +46,10 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+gem 'faker', '1.7.3'
+gem 'will_paginate',           '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  
+   before_action :logged_in_user, only: [:edit, :update, :index]
+   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
    def show
      puts params
     @user = User.find(params[:id])
@@ -12,7 +18,7 @@ class UsersController < ApplicationController
   def create
     puts params
     @user = User.new(user_params)
-     if @user.save
+    if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -21,24 +27,44 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+    def edit
+    @user = User.find(params[:id])
+  end
+
   def User.new_token
     SecureRandom.urlsafe_base64
   end
-#   def create
-#   @user = User.new(user_params)    
-#     if @user.save
-#       # Handle a successful save.
-#     else
-#       render 'new'
-#     end
-  
-# end
-private
+
+    def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       flash[:success] = "Profile updated"
+       redirect_to @user
+     
+    else
+      render 'edit'
+    end
+  end
+
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+     def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
   
